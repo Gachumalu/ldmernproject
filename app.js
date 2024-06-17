324d68ad4fc038459405da2357fc83fe7d19d7af
godson malu <godsonm2000@gmail.com>
2024-06-17 05:18:16 -0500

    first commit

--- app.js
+++ app.js
@@ -38,6 +38,10 @@ app.get('/signup',(req,res)=>{
 // 3: step heroku
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
 }
 
